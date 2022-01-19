617185ac8b7b49965df1744f295bee1ee909a3ef
Reuben Cleetus <rcleetus@gmail.com>
2022-01-19 14:56:01 -0500

    Local-only mode

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from rich.console import Console
 import time
 import sys
 import logging
+from rich import print
+from rich.panel import Panel
 log_format = '%(asctime)s %(filename)s: %(message)s'
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format=log_format, datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
@@ -24,7 +26,9 @@ _terraform_file = f"{str(pathlib.Path().resolve())}/{_working_dir}/{_clone_dsop_
 @click.command()
 @click.option('--config', default="", help="Path and Filename of Custom Cofig File Location. If not provided, './config/config.json' is assumed")
 @click.option('--settings', default="n", help="Print app config settings (these configure Azure Cloud Login and Terraform)")
-def main(config:str = "", settings=""):
+@click.option('--deploy', default="y", help="Only run local splicing of Terraform values. If 'y', the Terrform deployment is also performed (default)")
+def main(config:str = "", settings:str="", deploy:str="n"):
+    print(Panel.fit("PyBuilder - The Pythonic Big Bang Deployment Tool\nReuben Cleetus - reuben@cleet.us"))
     if config.strip() == "" : _app = AppSettings()
     if settings.strip() != "n":
         _app.print_settings_json()
@@ -40,16 +44,18 @@ def main(config:str = "", settings=""):
             splice_file_token(_terraform_file, "location", _app.settings["general"]["location"])
             splice_file_token(_terraform_file, "server_public_ip", _app.settings["connectivity"]["server_public_ip"])
             splice_file_token(_terraform_file, "server_open_ssh_public", _app.settings["connectivity"]["server_open_ssh_public"])
-        with console.status("Initializing Azure-CLI login...", spinner="earth"):
-            logger.debug("Initializing Azure Cloud")
-            _stream.do_cloud_login()
-        with console.status("Initializing Terraform...", spinner="earth"):
-            logger.debug("Initializing Terraform")
-            _stream._run_terraform_init()
-        with console.status("Running Terraform... This may take a while.", spinner="earth"):
-            logger.debug("Running Terraform")
-            _stream._run_terraform()
-        pass
+            _stream._cout_success("Completed Terraform Token splicing!")
+
+        if deploy.strip() == "y":
+            with console.status("Initializing Azure-CLI login...", spinner="earth"):
+                logger.debug("Initializing Azure Cloud")
+                _stream.do_cloud_login()
+            with console.status("Initializing Terraform...", spinner="earth"):
+                logger.debug("Initializing Terraform")
+                _stream._run_terraform_init()
+            with console.status("Running Terraform... This may take a while.", spinner="earth"):
+                logger.debug("Running Terraform")
+                _stream._run_terraform()
     else:
         #VNet Customization
         pass

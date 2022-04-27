64a846544c7c4b0b34ae43d3e4514c4cd6869fc3
Tim Meyers <metnem@gmail.com>
2022-04-27 15:26:42 -0400

    add support for node_count to aks

--- command/dsop_aks.py
+++ command/dsop_aks.py
@@ -59,6 +59,8 @@ def apply(
             splice_file_token(_terraform_file,"cluster_name", _app_settings.settings["general"]["cluster_name"])
             splice_file_token(_terraform_file, "cloud", _app_settings.settings["general"]["cloud"])
             splice_file_token(_terraform_file, "location", _app_settings.settings["general"]["location"])
+            splice_file_token(_terraform_file, "node_count", _app_settings.settings["cluster-size"]["agent_instance_count"])
+            splice_file_token(_terraform_file, "max_count", _app_settings.settings["cluster-size"]["agent_instance_count"])
             splice_file_token(_terraform_file, "server_public_ip", _app_settings.settings["connectivity"]["server_public_ip"])
             splice_file_token(_terraform_file, "server_open_ssh_public", _app_settings.settings["connectivity"]["server_open_ssh_public"])
             splice_file_list(_terraform_file, "aad_group_ids", _app_settings.settings["custom_aad_settings_for_aks"]["aad_group_ids"])
